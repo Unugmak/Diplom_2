a3d0218a95fa4c68a57fa038aab1727fc4f52323
Mikhail Kazakou <mixail.kazakov.1989@yandex.ru>
2023-10-09 21:35:36 +0300

    использовал lombok в UserNewData

--- src/main/java/users/UserNewData.java
+++ src/main/java/users/UserNewData.java
@@ -1,30 +1,14 @@
 package users;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.apache.commons.lang3.RandomStringUtils;
 
+@Data
+@AllArgsConstructor
 public class UserNewData {
     private String email;
     private String name;
-
-    public UserNewData(String email, String name) {
-        this.email = email;
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public static UserNewData generateNewUserDataRandom(){
         String email = RandomStringUtils.randomAlphabetic(6) + "@yandex.ru";
